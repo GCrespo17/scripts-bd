d3180b5971744d699185ab795d5a6ce41dd84f6d
Gabriel <gcrespo1705@gmail.com>
2025-06-03 16:59:04 -0400

    Added FK constraints and organized attribute order

--- create_table.sql
+++ create_table.sql
@@ -30,10 +30,11 @@
 
 CREATE TABLE LUGARES(
     id_lugar NUMBER DEFAULT seq_lugar.NEXTVAL PRIMARY KEY,
-    id_lugar_padre NUMBER, --FK
     nombre VARCHAR2(100) NOT NULL,
     tipo VARCHAR2(10) NOT NULL,
+    id_lugar_padre NUMBER, --FK
     continente VARCHAR2(15),
+    CONSTRAINT fk_id_lugar_padre FOREIGN KEY(id_lugar_padre) REFERENCES LUGARES(id_lugar),
     CONSTRAINT check_tipo CHECK(tipo IN('PAIS', 'CIUDAD')),
     CONSTRAINT check_continente CHECK(
         (tipo = 'PAIS' AND continente IN('AMERICA', 'EUROPA', 'ASIA', 'OCEANIA', 'AFRICA', 'ANTARTICA')) OR 
@@ -70,7 +71,7 @@ CREATE TABLE EMPLEADOS_VIGILANTE_MANT(
 
 CREATE TABLE ARTISTAS(
     id_artista NUMBER DEFAULT seq_artista.NEXTVAL PRIMARY KEY,
-    id_lugar NUMBER NOT NULL, --FK
+    resumen_caracteristicas VARCHAR2(300) NOT NULL,
     primer_nombre VARCHAR2(30),
     segundo_nombre VARCHAR2(30),
     primer_apellido VARCHAR2(30),
@@ -78,34 +79,39 @@ CREATE TABLE ARTISTAS(
     fecha_nacimiento DATE,
     fecha_difuncion DATE,
     apodo VARCHAR2(50),
-    resumen_caracteristicas VARCHAR2(300) NOT NULL,
+    id_lugar NUMBER, --FK
     CONSTRAINT check_fechas CHECK(
         fecha_nacimiento IS NULL OR
         fecha_difuncion IS NULL OR
         fecha_difuncion >= fecha_nacimiento
-    )
+    ),
+    CONSTRAINT fk_id_lugar_artista FOREIGN KEY(id_lugar) REFERENCES LUGARES(id_lugar)
 );
 
 CREATE TABLE ARTISTAS_OBRAS(
     id_obra NUMBER NOT NULL, --PK FK
     id_artista NUMBER NOT NULL, --PK FK
+    CONSTRAINT fk_obra_artista FOREIGN KEY(id_obra) REFERENCES OBRAS(id_obra),
+    CONSTRAINT fk_artista_obra FOREIGN KEY(id_artista) REFERENCES ARTISTAS(id_artista),
     CONSTRAINT pk_artistas_obras PRIMARY KEY(id_obra, id_artista)
 );
 
 CREATE TABLE EMPLEADOS_PROFESIONALES(
     id_empleado NUMBER DEFAULT seq_empleado_profesional.NEXTVAL PRIMARY KEY,
-    primer_nombre VARCHAR2(30) NOT NULL,
-    segundo_nombre VARCHAR2(30),
-    primer_apellido VARCHAR2(30) NOT NULL,
-    segundo_apellido VARCHAR2(30),
-    fecha_nacimiento DATE NOT NULL,
     doc_identidad NUMBER NOT NULL UNIQUE,
-    contacto NUMBER NOT NULL
+    primer_nombre VARCHAR2(30) NOT NULL,
+    primer_apellido VARCHAR2(30) NOT NULL,
+    fecha_nacimiento DATE NOT NULL,
+    contacto NUMBER NOT NULL,
+    segundo_nombre VARCHAR2(30),
+    segundo_apellido VARCHAR2(30)
 );
 
 CREATE TABLE EMPLEADOS_IDIOMAS(
     id_empleado NUMBER NOT NULL, --PK FK
     id_idioma NUMBER NOT NULL, --PK FK
+    CONSTRAINT fk_empleado_idioma FOREIGN KEY(id_empleado) REFERENCES EMPLEADOS_PROFESIONALES(id_empleado),
+    CONSTRAINT fk_idioma_empleado FOREIGN KEY(id_idioma) REFERENCES IDIOMAS(id_idioma),
     CONSTRAINT pk_empleados_idiomas PRIMARY KEY(id_empleado, id_idioma)
 );
 
@@ -115,25 +121,29 @@ CREATE TABLE FORMACIONES(
     titulo VARCHAR2(50) NOT NULL,
     anio NUMBER NOT NULL,
     descripcion_espec VARCHAR2(300) NOT NULL,
+    CONSTRAINT fk_empleado_prof_formacion FOREIGN KEY(id_empleado_prof) REFERENCES EMPLEADOS_PROFESIONALES(id_empleado),
     CONSTRAINT pk_formaciones PRIMARY KEY(id_formacion, id_empleado_prof)
 );
 
 CREATE TABLE MUSEOS(
     id_museo NUMBER DEFAULT seq_museo.NEXTVAL PRIMARY KEY, 
     id_lugar NUMBER NOT NULL, --FK
-    nombre VARCHAR2(100),
+    nombre VARCHAR2(100) NOT NULL,
     fecha_fundacion DATE NOT NULL,
-    mision VARCHAR2(300) NOT NULL
+    mision VARCHAR2(300) NOT NULL,
+    CONSTRAINT fk_lugar_museo FOREIGN KEY(id_lugar) REFERENCES LUGARES(id_lugar)
 );
 
 CREATE TABLE EST_FISICA(
     id_est NUMBER DEFAULT seq_est_fisica.NEXTVAL NOT NULL, --PK
     id_museo NUMBER NOT NULL, --PK FK
-    id_est_padre NUMBER, --FK
     nombre VARCHAR2(100) NOT NULL,
     tipo VARCHAR2(12) NOT NULL,
     descripcion VARCHAR2(300),
     direccion_edificio VARCHAR2(300),
+    id_est_padre NUMBER, --FK
+    CONSTRAINT fk_museo_est_fisica FOREIGN KEY(id_museo) REFERENCES MUSEOS(id_museo),
+    CONSTRAINT fk_est_fisica_padre FOREIGN KEY(id_est_padre, id_museo) REFERENCES EST_FISICA(id_est, id_museo),
     CONSTRAINT check_tipo_est_fisica CHECK(tipo IN('EDIFICIO', 'PISO', 'AREA')),
     CONSTRAINT pk_est_fisica PRIMARY KEY(id_est, id_museo)
 );
@@ -144,6 +154,8 @@ CREATE TABLE ASIGNACIONES_MES(
     id_museo NUMBER NOT NULL, --PK FK
     id_vig_mant NUMBER NOT NULL, --PK FK
     turno VARCHAR2(15) NOT NULL,
+    CONSTRAINT fk_est_museo_asignacion FOREIGN KEY(id_est, id_museo) REFERENCES EST_FISICA(id_est, id_museo),
+    CONSTRAINT fk_vig_mant_asignacion FOREIGN KEY(id_vig_mant) REFERENCES EMPLEADOS_VIGILANTE_MANT(id_vig_mant),
     CONSTRAINT check_turno CHECK(turno IN('MATUTINO', 'VESPERTINO', 'NOCTURNO')),
     CONSTRAINT pk_asignaciones_mes PRIMARY KEY(mes_anio, id_est, id_museo, id_vig_mant)
 );
@@ -154,6 +166,7 @@ CREATE TABLE SALAS_EXP(
     id_museo NUMBER NOT NULL, --PK FK
     nombre VARCHAR2(50),
     descripcion VARCHAR2(300),
+    CONSTRAINT fk_est_museo_sala FOREIGN KEY(id_est, id_museo) REFERENCES EST_FISICA(id_est, id_museo),
     CONSTRAINT pk_salas_exp PRIMARY KEY(id_sala, id_est, id_museo)
 );
 
@@ -168,6 +181,7 @@ CREATE TABLE EXPOSICIONES_EVENTOS(
     costo_persona NUMBER(8, 2),
     cant_visitantes NUMBER,
     institucion_educativa VARCHAR2(250),
+    CONSTRAINT fk_sala_est_museo_exposicion FOREIGN KEY(id_sala, id_est, id_museo) REFERENCES SALAS_EXP(id_sala, id_est, id_museo),
     CONSTRAINT check_fechas_exp CHECK(fecha_fin<=fecha_inicio),
     CONSTRAINT pk_expo_eventos PRIMARY KEY(id_expo, id_sala, id_est, id_museo)
 );
@@ -178,6 +192,7 @@ CREATE TABLE CIERRES_TEMPORALES(
     id_est NUMBER NOT NULL, --PK FK
     id_museo NUMBER NOT NULL, --PK FK
     fecha_fin DATE,
+    CONSTRAINT fk_sala_est_museo_cierretemp FOREIGN KEY(id_sala, id_est, id_museo) REFERENCES SALAS_EXP(id_sala, id_est, id_museo),
     CONSTRAINT check_fechas_cierres CHECK (fecha_fin IS NULL OR fecha_fin >= fecha_inicio),
     CONSTRAINT pk_cierres_tem PRIMARY KEY(fecha_inicio, id_sala, id_est, id_museo)
 );
@@ -185,11 +200,13 @@ CREATE TABLE CIERRES_TEMPORALES(
 CREATE TABLE EST_ORGANIZACIONAL(
     id_est_org NUMBER DEFAULT seq_est_organizacional.NEXTVAL NOT NULL, --PK
     id_museo NUMBER NOT NULL, --PK FK
-    id_est_org_padre NUMBER, --FK
     nombre VARCHAR2(50) NOT NULL, 
     tipo VARCHAR2(20) NOT NULL,
     nivel NUMBER NOT NULL,
     descripcion VARCHAR2(300) NOT NULL,
+    id_est_org_padre NUMBER, --FK
+    CONSTRAINT fk_museo_est_org FOREIGN KEY(id_museo) REFERENCES MUSEOS(id_museo),
+    CONSTRAINT fk_est_org_padre FOREIGN KEY(id_est_org_padre, id_museo) REFERENCES EST_ORGANIZACIONAL(id_est_org, id_museo),
     CONSTRAINT check_tipo_est_org CHECK(tipo IN('DIRECCION', 'DEPARTAMENTO', 'SECCION')),
     CONSTRAINT pk_est_organizacional PRIMARY KEY(id_est_org, id_museo)
 );
@@ -202,6 +219,7 @@ CREATE TABLE COLECCIONES_PERMANENTES(
     caracteristicas VARCHAR2(300) NOT NULL,
     palabra_clave VARCHAR2(20) NOT NULL,
     orden_recorrido NUMBER NOT NULL,
+    CONSTRAINT fk_est_org_museo_coleccion FOREIGN KEY(id_est_org, id_museo) REFERENCES EST_ORGANIZACIONAL(id_est_org, id_museo),
     CONSTRAINT pk_colecciones PRIMARY KEY(id_coleccion, id_est_org, id_museo)
 );
 
@@ -212,6 +230,8 @@ CREATE TABLE SALAS_COLECCIONES(
     id_sala NUMBER NOT NULL, --PK FK
     id_est_fisica NUMBER NOT NULL, --PK FK
     orden NUMBER NOT NULL,
+    CONSTRAINT fk_sala_estfis_museo_sala FOREIGN KEY(id_sala, id_est_org, id_museo) REFERENCES SALAS_EXP(id_sala, id_est, id_museo),
+    CONSTRAINT fk_coleccion_estorg_museo_coleccion FOREIGN KEY(id_coleccion, id_est_org, id_museo) REFERENCES COLECCIONES_PERMANENTES(id_coleccion, id_est_org, id_museo),
     CONSTRAINT pk_salas_colecciones PRIMARY KEY(id_coleccion, id_est_org, id_museo, id_sala, id_est_fisica)
 );
 
@@ -220,8 +240,10 @@ CREATE TABLE HIST_EMPLEADOS(
     id_est_org NUMBER NOT NULL, --PK FK
     id_museo NUMBER NOT NULL, --PK FK
     id_empleado_prof NUMBER NOT NULL, --PK FK
-    fecha_fin DATE,
     cargo VARCHAR2(50) NOT NULL,
+    fecha_fin DATE,
+    CONSTRAINT fk_est_org_museo_hist_emp FOREIGN KEY(id_est_org, id_museo) REFERENCES EST_ORGANIZACIONAL(id_est_org, id_museo),
+    CONSTRAINT fk_empleado_hist_emp FOREIGN KEY(id_empleado_prof) REFERENCES EMPLEADOS_PROFESIONALES(id_empleado),
     CONSTRAINT check_fechas_hist_emp CHECK(fecha_fin IS NULL OR fecha_fin >= fecha_inicio),
     CONSTRAINT check_cargo CHECK(cargo IN('RESTAURADOR', 'CURADOR', 'DIRECTOR', 'ADMINISTRATIVO')),
     CONSTRAINT pk_hist_empleados PRIMARY KEY(fecha_inicio, id_est_org, id_museo, id_empleado_prof)
@@ -237,11 +259,15 @@ CREATE TABLE HIST_OBRAS_MOV(
     id_museo NUMBER NOT NULL,   --FK sala y est org
     id_est_fis NUMBER NOT NULL, --FK sala
     fecha_entrada DATE NOT NULL,
-    fecha_salida DATE,
     tipo_adquisicion VARCHAR2(20) NOT NULL,
     destacada VARCHAR2(2) NOT NULL,
+    fecha_salida DATE,
     orden_recorrido NUMBER,
     valor_monetario NUMBER(*, 4),
+    CONSTRAINT fk_obra_hist_obra FOREIGN KEY(id_obra) REFERENCES OBRAS(id_obra),
+    CONSTRAINT fk_coleccion_hist_obra FOREIGN KEY(id_coleccion, id_est_org, id_museo) REFERENCES COLECCIONES_PERMANENTES(id_coleccion, id_est_org, id_museo),
+    CONSTRAINT fk_sala_hist_obra FOREIGN KEY(id_sala, id_est_fis, id_museo) REFERENCES SALAS_EXP(id_sala, id_est, id_museo),
+    CONSTRAINT fk_empleado_hist_obra FOREIGN KEY(id_empleado) REFERENCES EMPLEADOS_PROFESIONALES(id_empleado),
     CONSTRAINT check_fechas_hist_ob_mov CHECK(fecha_salida IS NULL OR fecha_salida >= fecha_entrada),
     CONSTRAINT check_tipo_adq CHECK(tipo_adquisicion IN('COMPRADA', 'DONADA', 'COMPRADA A MUSEO', 'DONADA POR MUSEO')),
     CONSTRAINT check_destacada CHECK(destacada IN('SI', 'NO')),
@@ -255,6 +281,7 @@ CREATE TABLE PROGRAMAS_MANT(
     actividad VARCHAR2(300) NOT NULL, 
     frecuencia VARCHAR2(100) NOT NULL,
     tipo_responsable VARCHAR2(20) NOT NULL,
+    CONSTRAINT fk_catalogo_mant FOREIGN KEY(id_catalogo, id_obra) REFERENCES HIST_OBRAS_MOV(id_catalogo_museo, id_obra),
     CONSTRAINT check_tipo_responsable_programa_mant CHECK(tipo_responsable IN('RESTAURADOR', 'CURADOR')),
     CONSTRAINT pk_programas_mant PRIMARY KEY(id_mant, id_catalogo, id_obra)
 );
@@ -268,6 +295,8 @@ CREATE TABLE MANTENIMIENTOS_OBRAS_REALIZADOS(
     fecha_inicio DATE NOT NULL,
     fecha_fin DATE,  
     observaciones VARCHAR2(300),
+    CONSTRAINT fk_mantenimiento_mant_realizado FOREIGN KEY(id_mant, id_catalogo, id_obra) REFERENCES PROGRAMAS_MANT(id_mant, id_catalogo, id_obra),
+    CONSTRAINT fk_empleado_mant_realizado FOREIGN KEY(id_empleado) REFERENCES EMPLEADOS_PROFESIONALES(id_empleado),
     CONSTRAINT check_fecha_mant_realizados CHECK(fecha_fin IS NULL OR fecha_fin >= fecha_inicio),
     CONSTRAINT pk_mant_ralizado PRIMARY KEY(id_mant_realizado, id_mant, id_catalogo, id_obra, id_empleado)
 );
@@ -277,6 +306,7 @@ CREATE TABLE HORARIOS(
     id_museo NUMBER NOT NULL,  --PK FK
     hora_inicio DATE NOT NULL, 
     hora_cierre DATE NOT NULL, 
+    CONSTRAINT fk_museo_horario FOREIGN KEY(id_museo) REFERENCES MUSEOS(id_museo),
     CONSTRAINT check_dia CHECK(dia>=1 AND dia<=7),
     CONSTRAINT check_horas_validas CHECK (
         TO_CHAR(hora_inicio, 'HH24:MI:SS') < TO_CHAR(hora_cierre, 'HH24:MI:SS')
@@ -290,6 +320,7 @@ CREATE TABLE TICKETS(
     fecha_hora_emision DATE NOT NULL,
     tipo VARCHAR2(15) NOT NULL,
     precio NUMBER (*, 4) NOT NULL,
+    CONSTRAINT fk_museo_ticket FOREIGN KEY(id_museo) REFERENCES MUSEOS(id_museo),
     CONSTRAINT check_tipo_ticket CHECK(tipo IN('ESTUDIANTE', 'INFANTIL', 'ADULTO')),
     CONSTRAINT pk_ticket PRIMARY KEY(id_num_ticket, id_museo) 
 );
@@ -298,6 +329,7 @@ CREATE TABLE HIST_MUSEOS(
     anio NUMBER(4) NOT NULL, --PK
     id_museo NUMBER NOT NULL, --PK FK
     hecho VARCHAR2(300) NOT NULL,
+    CONSTRAINT fk_museo_hist_museo FOREIGN KEY(id_museo) REFERENCES MUSEOS(id_museo),
     CONSTRAINT pk_hist_museo PRIMARY KEY(anio, id_museo)
 );
 
@@ -307,6 +339,7 @@ CREATE TABLE TIPO_TICKETS(
     precio NUMBER(*, 2) NOT NULL,
     tipo VARCHAR2(20) NOT NULL,
     fecha_fin DATE,
+    CONSTRAINT fk_museo_tipo_ticket FOREIGN KEY(id_museo) REFERENCES MUSEOS(id_museo),
     CONSTRAINT check_fechas_tipo_ticket CHECK(fecha_fin IS NULL OR fecha_fin>=fecha_inicio),
     CONSTRAINT check_tipo_hist_ticket CHECK(tipo IN('ESTUDIANTE', 'INFANTIL', 'ADULTO')),
     CONSTRAINT pk_tipo_tickets PRIMARY KEY(fecha_inicio, id_museo)
